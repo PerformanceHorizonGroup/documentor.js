3994b93ec808efef2b0a635656d47566a77a0815
Georgi Kostov <p_e_a@gbg.bg>
2013-11-29 15:15:05 +0200

    updated build script and added to npm's pre-publish

--- build/build.js
+++ build/build.js
@@ -1,64 +1,112 @@
 var fs=require('fs'),
-	outputFileName=__dirname+require('path').sep+'documentor.js',
-	files=[
-		'../lib/util.js',
-		'../lib/events.js',
-		
-		'../src/Documentor.js',
-		'../src/SourceLoader.js',
-		'../src/SourceProcessor.js',
-		'../src/render/DocumentationRenderer.js',
-		
-		'../src/PHGDoc/PHGSourceProcessor.js',
-		'../src/PHGDoc/PHGDocRenderer.js'
-	];
+	outputPath=__dirname+require('path').sep;
 
 console.log('working...');
-var ws=fs.createWriteStream(outputFileName);
-ws.write('/*!\n\
- * Documentor.js\n\
- * Copyright(c) 2012-2013 Georgi Kostov <p_e_a@gbg.bg>, http://performancehorizon.com\n\
- * https://github.com/PerformanceHorizonGroup/documentor.js\n\
- * MIT Licensed\n\
- */\n\
-');
 
-function bundle(){
-	var fn=files.shift();
-	if(fn){
-		var rs=fs.createReadStream(fn);
-		rs.on('open', function (){
-			rs.pipe(ws, {
-				end:false
+function bundle(ws, files, cb){
+	function bundleNext(){
+		var fn=files.shift();
+		if(fn){
+			var rs=fs.createReadStream(outputPath+fn);
+			rs.on('open', function (){
+				rs.pipe(ws, {
+					end:false
+				});
+				rs.on('end', bundleNext);
 			});
-			rs.on('end', bundle);
-		});
-	}else{
-		ws.end();
-		
-		// try to pack it
-		try{
-			require('yuicompressor').compress(outputFileName, function (err, data, extra){
-				if(err){
-					console.log(err);
-					console.log('failed compressing: '+outputFileName);
-					return;
-				}else{
-					var packedFileName=outputFileName.replace(/\.js$/, '.min.js');
-					fs.writeFile(packedFileName, data, function (err){
-						if(err){
-							console.log(err);
-							console.log('failed writing: '+packedFileName);
-							return;
-						}else
-							console.log('packed to '+packedFileName);
-					});
-				}
-			});
-		}catch(e){
-			console.log('[WARN] could not create minified!');
+		}else{
+			cb && cb();
 		}
+	}
+	bundleNext();
+}
+function minify(fileName, cb){
+	try{
+		var packedFileName=fileName.replace(/\.js$/, '.min.js');
+		require('yuicompressor').compress(fileName, function (err, data, extra){
+			if(err){
+				console.log(err);
+				console.log('failed compressing: '+fileName);
+				return;
+			}else{
+				fs.writeFile(packedFileName, data, function (err){
+					if(err){
+						console.log(err);
+						console.log('failed writing: '+packedFileName);
+						return;
+					}else
+						console.log('packed to '+packedFileName);
+					cb && cb(err);
+				});
+			}
+		});
+		console.log('compressing '+fileName+' ...');
+	}catch(e){
+		console.log('[WARN] could not create minified '+packedFileName+'!');
+		cb && cb(e);
 	}
 }
 
-bundle();
+(function (){
+	console.log('creating full package ...');
+	var outputFileName=outputPath+'documentor.js',
+		ws=fs.createWriteStream(outputFileName),
+		files=[
+			'../lib/util.js',
+			'../lib/events.js',
+			
+			'../src/Documentor.js',
+			'../src/SourceLoader.js',
+			'../src/SourceProcessor.js',
+			'../src/render/DocumentationRenderer.js',
+			
+			'../src/PHGDoc/PHGSourceProcessor.js',
+			'../src/PHGDoc/PHGDocRenderer.js'
+		];
+	ws.write('/*!\n\
+	 * Documentor.js\n\
+	 * Copyright(c) 2012-2013 Georgi Kostov <p_e_a@gbg.bg>, http://performancehorizon.com\n\
+	 * https://github.com/PerformanceHorizonGroup/documentor.js\n\
+	 * MIT Licensed\n\
+	 */\n\
+	');
+	
+	bundle(ws, files, function (){
+		ws.end();
+		
+		// try to pack it
+		minify(outputFileName);
+	});
+}());
+
+(function (){
+	console.log('creating PHGDoc package ...');
+	var outputFileName=outputPath+'PHGDoc.js',
+		ws=fs.createWriteStream(outputFileName),
+		files=[
+			'../lib/util.js',
+			'../lib/events.js',
+			
+//			'../src/Documentor.js',
+//			'../src/SourceLoader.js',
+//			'../src/SourceProcessor.js',
+			'../src/render/DocumentationRenderer.js',
+			
+//			'../src/PHGDoc/PHGSourceProcessor.js',
+			'../src/PHGDoc/PHGDocRenderer.js'
+		];
+	ws.write('/*!\n\
+	 * PHGDoc.js\n\
+	 * Copyright(c) 2012-2013 Georgi Kostov <p_e_a@gbg.bg>, http://performancehorizon.com\n\
+	 * https://github.com/PerformanceHorizonGroup/documentor.js\n\
+	 * MIT Licensed\n\
+	 */\n\
+	');
+	
+	bundle(ws, files, function (){
+		ws.end();
+		
+		// try to pack it
+		minify(outputFileName);
+	});
+}());
